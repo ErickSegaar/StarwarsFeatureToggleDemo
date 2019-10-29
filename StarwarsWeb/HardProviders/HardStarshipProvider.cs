ead112991faef5bddbb3075225f457851023bca8
Erick Segaar <zwarebats@gmail.com>
2019-10-29 07:27:59 +0100

    Improving HardStarshipProvider

--- StarwarsWeb/HardProviders/HardStarshipProvider.cs
+++ StarwarsWeb/HardProviders/HardStarshipProvider.cs
@@ -5,15 +5,16 @@ namespace StarwarsWeb.HardProviders
 {
     public class HardStarshipProvider
     {
-        public async Task<Starships> GetStarships()
+        public Task<Starships> GetStarships()
         {
-            return await Task.Run(() => new Starships { 
-                List = new List<Starship> { 
+            return Task.FromResult(new Starships
+            {
+                List = new List<Starship> {
                     new Starship { Name = "Executor", Model = "Executor-class star dreadnought", Manufacturer = "Kuat Drive Yards, Fondor Shipyard" },
                     new Starship { Name = "Sentinel-class landing craft", Model = "Sentinel-class landing craft", Manufacturer = "Kuat Drive Yards, Fondor Shipyard" },
                     new Starship { Name ="Death Star", Model = "Playmobil", Manufacturer = "Jean-Luc Piccard" }
                 }
-            } );
+            });
         }
     }
 }
